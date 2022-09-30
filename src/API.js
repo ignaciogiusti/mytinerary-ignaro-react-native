f851979f11a7ae8a1c99aac31a39a248b73b10d6
ignaciogiusti <110680518+ignaciogiusti@users.noreply.github.com>
2022-09-30 13:50:03 -0300

    Delete API.js

--- src/API.js
+++
@@ -1,5 +0,0 @@
-const urlAPI = 'https://my-tinerary-back-ignaro.herokuapp.com'
-// const urlAPI = 'https://mytinerary-ignaro-back.herokuapp.com'
-// const urlAPI = 'http://localhost:4000'
-
-export default urlAPI
