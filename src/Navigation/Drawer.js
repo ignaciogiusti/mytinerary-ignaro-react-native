91c2f998e02628bb7f3a789f36d7f444fb1a902b
ignaciogiusti <nachogiusti94@gmail.com>
2022-09-30 12:03:38 -0300

    Re-created SignInScreen.js and used it in Drawer.js

--- src/Navigation/Drawer.js
+++ src/Navigation/Drawer.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import HomeScreen from '../Screens/HomeScreen';
-import CitiesScreen from '../Screens/CitiesScreen';
+import React from 'react';
+import HomeScreen from '../Screens/HomeScreen'
+import CitiesScreen from '../Screens/CitiesScreen'
+import SignInScreen from '../Screens/SignInScreen';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const DrawerNavigation = createDrawerNavigator();
@@ -9,6 +10,10 @@ export default function DrawerNavigator() {
         <DrawerNavigation.Navigator>
             <DrawerNavigation.Screen name='Home' component={HomeScreen}/>
             <DrawerNavigation.Screen name='Cities' component={CitiesScreen}/>
+            {/* <DrawerNavigation.Screen name='New City' component={NewCityScreen}/> */}
+            {/* <DrawerNavigation.Screen name='Edit City' component={EditCityScreen}/> */}
+            <DrawerNavigation.Screen name='Sign In' component={SignInScreen}/>
+            {/* <DrawerNavigation.Screen name='Profile' component={ProfileScreen}/> */}
         </DrawerNavigation.Navigator>
     )
 }
