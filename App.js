54de01cd3696c60864e5c176d649d98a8314fb9c
ignaciogiusti <nachogiusti94@gmail.com>
2022-09-30 12:00:59 -0300

    Imported and implemented use of store.js with Provider tag

--- App.js
+++ App.js
@@ -4,36 +4,17 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
-import citiesAPI from './redux/citiesAPI';
-import store from '../mitinerary-alessandro-giusti/src/store';
+import citiesAPI from './src/features/citiesAPI';
+import store from './src/store';
 import DrawerNavigator from './src/Navigation/Drawer';
 
 export default function App() {
-  // const reduxStore = configureStore({ reducer: citiesAPI })
   return (
-//     <View style={styles.container}>
-//       <View>
-//         <Text>HomeScreen</Text>
-//         <Text>HomeScreen</Text>
-//         <Text>HomeScreen</Text>
-//       </View>
-//     </View>
-//   )
-// }
 
     <Provider store={store} >
       <NavigationContainer>
         <DrawerNavigator />
       </NavigationContainer>
-      // </Provider>
+      </Provider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
