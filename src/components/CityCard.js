b3c7edbd62518b0ba545da4fdda8087f1473e93a
LautiAlessa <alessandrolautaror@gmail.com>
2022-09-30 13:37:15 -0300

    modified component for carousel

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import urlAPI from '../API';
+// import '../styles/Cities.css';
+// import axios from 'axios'
+// import { useEffect, useState } from 'react';
+// import { Link as LinkRouter } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { fetchFromServer } from '../features/citiesSlice';
+import { useGetAllCitiesQuery } from '../features/citiesAPI';
 
-const city = {
-    photo: 'http://mytinerary-ignaro.herokuapp.com/static/media/Venice.2dab1fd4b1b567bcc5db.jpg',
-    city: 'City'
-};
+// const city = {
+//     photo: 'http://mytinerary-ignaro.herokuapp.com/static/media/Venice.2dab1fd4b1b567bcc5db.jpg',
+//     city: 'City'
+// };
 export default function CityCard() {
+
+    const { 
+        data: cities,
+        error,
+        isLoading,
+        isSuccess,
+        isFailed
+    } = useGetAllCitiesQuery()
+    
+
+    return (
+        <>
+            <View className='citiesPageContainer'>
+                {cities.map((city) => (
+                    // <LinkRouter key={city._id} to={`/citydetails/${city._id}`}>
+                        <View className='City-container'>
+                            <img className="City-img" src={city.photo} />
+                            <h3 className='City-text text-center'>{city.city}</h3>
+                        </View>
+                    // </LinkRouter>
+                ))}
+            </View>
+        </>
+    )
     return (
         <View style={styles.container}>
             <Image source={city.photo} resizeMode="cover" style={styles.test} />
@@ -15,6 +47,7 @@ export default function CityCard() {
         </View>
     )
 }
+
 
 //----------------------------------------------------
 const styles = StyleSheet.create({
