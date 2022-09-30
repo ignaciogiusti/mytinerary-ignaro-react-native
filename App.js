e62842fe806e5ced9eac09bd54e3ea08061bce83
ignaciogiusti <nachogiusti94@gmail.com>
2022-09-30 11:28:06 -0300

    Removed useless commented lines

--- App.js
+++ App.js
@@ -6,19 +6,11 @@ import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 import citiesAPI from './redux/citiesAPI';
 import DrawerNavigator from './src/Navigation/Drawer';
+import StackNavigate from './src/Navigation/Stack';
 
 export default function App() {
   // const reduxStore = configureStore({ reducer: citiesAPI })
   return (
-//     <View style={styles.container}>
-//       <View>
-//         <Text>HomeScreen</Text>
-//         <Text>HomeScreen</Text>
-//         <Text>HomeScreen</Text>
-//       </View>
-//     </View>
-//   )
-// }
 
     // <Provider /* store={reduxStore} */>
       <NavigationContainer>
@@ -27,12 +19,3 @@ export default function App() {
       // </Provider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
