f40dc04228826c2106c5ab5fb09bd682ee0127c3
ignaciogiusti <nachogiusti94@gmail.com>
2022-09-30 14:42:35 -0300

    recreated API.js

---
+++ src/API.js
@@ -0,0 +1,5 @@
+// const urlAPI = 'https://my-tinerary-back-ignaro.herokuapp.com'
+// const urlAPI = 'https://mytinerary-ignaro-back.herokuapp.com'
+const urlAPI = 'http://localhost:4000'
+
+export default urlAPI
