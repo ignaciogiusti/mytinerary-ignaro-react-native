cd9ca0a282712ad2d8242c0a922d59494148e93a
ignaciogiusti <nachogiusti94@gmail.com>
2022-09-30 13:47:35 -0300

    changed tags to proper ones and changed styles to display images

--- src/Screens/CitiesScreen.js
+++ src/Screens/CitiesScreen.js
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from 'react'
 // import { Link as LinkRouter } from 'react-router-dom';
 import { useGetAllCitiesQuery } from '../features/citiesAPI';
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image, ImageBackground, TextInput } from 'react-native';
 
 export default function CitiesScreen() {
     const [search, setSearch] = useState("")
@@ -15,73 +15,57 @@ export default function CitiesScreen() {
     } = useGetAllCitiesQuery(search)
 
     return (
-        <View style={styles.container}>
+        <View>
             <StatusBar style="auto" />
             <ScrollView>
-                <View className='Main-Cities'>
-                    <View className='Search-Cities'>
-                        <input className='Input-Cities' type="text" name="" placeholder=' Search by city...' onChange={(city) => { setSearch(city.target.value) }} />
+                <View style={styles.container}>
+                    <View>
+                        <TextInput style={styles.input} type="text" name="" placeholder=' Search by city...' onChange={(city) => { setSearch(city.target.value) }} />
                     </View>
-                    <View className='citiesPageContainer'>
+                    <View>
                         {cities?.response?.map((city) => (
-                                <View className='City-container'>
-                                    <img className="City-img" src={city.photo} />
-                                    <h3 className='City-text text-center'>{city.city}</h3>
-                                </View>
+                            <View style={styles.cityContainer}>
+                                <Image resizeMode='cover' style={{
+                                    width: 300,
+                                    height: 300,
+                                    margin: 12
+                                    }} source={{ uri: city.photo }} />
+                                <Text style={styles.text}>{city.city}</Text>
+                            </View>
                         ))}
                     </View>
                 </View>
-                <Image source={{ uri: 'http://mytinerary-ignaro.herokuapp.com/img/logo.png' }} />
             </ScrollView>
         </View>
     )
 }
 
-
-// import urlAPI from '../API';
-// import CityCard from '../components/CityCard';
-// import React, { useState, useEffect } from 'react'
-// import { Link as LinkRouter } from 'react-router-dom';
-// import { useGetAllCitiesQuery } from '../features/citiesAPI';
-// import { StatusBar } from 'expo-status-bar';
-// import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
-
-// // El onChange es una función de tipo "escuchador" que se va a actualizar cada vez que el input cambie
-
-// export default function Cities() {
-//     const [search, setSearch] = useState("")
-
-//     const {
-//         data: cities
-//     } = useGetAllCitiesQuery(search)
-
-//     return (
-//         <>
-//             <view className='Main-Cities'>
-//                 <view className='Search-Cities'>
-//                     <input className='Input-Cities' type="text" name="" placeholder=' Search by city...' onChange={(city) => { setSearch(city.target.value) }} />
-//                 </view>
-//                 <view className='citiesPageContainer'>
-//                     {cities?.response?.map((city) => (
-//                         <LinkRouter className='decoration-none flex-center' key={city._id} to={`/citydetails/${city._id}`}>
-//                             <view className='City-container'>
-//                                 <img className="City-img" src={city.photo} />
-//                                 <h3 className='City-text text-center'>{city.city}</h3>
-//                             </view>
-//                         </LinkRouter>
-//                     ))}
-//                 </view>
-//             </view>
-//         </>
-//     )
-// }
-
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: '#1d1e23',
+        height: '500%',
+        width: '100%',
     },
+    cityContainer: {
+        backgroundColor: '#f5f5f5',
+        marginBottom: 10
+    },
+    text: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 16,
+        paddingBottom: 12,
+    },
+    input: {
+        height: 30,
+        borderWidth: 1,
+        borderRadius: 6,
+        padding: 10,
+        margin: 10,
+        backgroundColor: '#f5f5f5',
+        width: '100%',
+    }
 });
